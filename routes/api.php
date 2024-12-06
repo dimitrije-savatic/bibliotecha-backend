01f4538ce653b18aa48d5be989c330f928dd634d
dimitrije-savatic <dimitrije.savatic@gmail.com>
2024-12-06 20:06:55 +0100

    First get request done

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/', );
+Route::get('/', [\App\Http\Controllers\MainController::class, 'getBooks'])->name('getBooks');
